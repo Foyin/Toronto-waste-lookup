a63480e6bca5e4cb03b38fa8ebfbed28eef2498c
Foyin <Foyin>
2019-01-10 07:51:05 -0500

    entities fixed

--- public/main.js
+++ public/main.js
@@ -1,5 +1,4 @@
 $(function() { 
-  
   // Initialize variables
   var $window = $(window);
   var $button = $('button');
@@ -7,69 +6,86 @@ $(function() {
   var $searchButton = $("#search");
   var $searchRes = $("#searchRes");
   var $favList = $("#favList");
-  var $favItem = $("#favItem");
+  var $favItems = $("#favItems");
   var $title = $(".title");
-  var $mainPage = $('.main.page');
+  var $mainPage = $('.main');
   var reposjson;
   var enterKey = jQuery.Event("keydown");
   //enterKey.which = 13; //Enter key
 
   $mainPage.show();
   $searchField.focus();
-  
-  //Check if input field is empty every second
+
+  function main(){ 
+	  $("#searchRes > div").remove();
+	  $.when(
+	    $.getJSON("https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000", function(json) {
+		data = json.filter( obj => obj.keywords.toString().includes($searchField.val()) );
+	    })  
+	  ).then(function() {
+		    if (data) {
+			  for (i = 0; i < data.length; i++) {
+				$searchRes.append("<div class=grid >" +
+							"<div class=title><i class=addFav ></i> " + data[i].title + "</div>" +
+							"<div class=desc>" + decodeEntities(data[i].body) + "</div>" + 
+							"</div>");
+				$('.title i').addClass('fas fa-star');
+			  }
+				$(".addFav").click(function(){	
+					if($(this).parent().parent().parent().attr('id') === 'searchRes'){		    
+						$favItems.append($(this).parent().parent().clone(true, true));
+						$(this).css('color','green');
+						$("#favList #favItems i").css('color','green');
+	    					$("#favItems > div").removeDuplicates();
+					}
+
+					else if($(this).parent().parent().parent().attr('id') === 'favItems'){
+						$(this).parent().parent().remove();
+					}
+				});
+
+		    }
+		    else {
+			console.log("Error JSON file not found");
+		    }
+	    
+	   });
+  }
+
+  //Check if input field is empty
   $searchField.keyup(function(){
 	if (!$searchField.val()){
 		$("#searchRes > div").remove();
 	}
   });
 
-  function main(){ 
-	  $("#searchRes > div").remove();
-	  var numEntries = 10;
-	  $.when(
-	    $.getJSON("https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000", function(json) {
-		data = json.filter( obj => obj.keywords.toString().includes($searchField.val()) );
-	    })  
-	  ).then(function() {
-	    if (data) {
-		  for (i = 0; i < data.length; i++) {
-			$searchRes.append("<div class=grid >" +
-						"<div class=title><i class=addFav ></i> " + data[i].title + "</div>" +
-						"<div class=desc>" + data[i].body + "</div>" + 
-						"</div>");
-			$('.title i').addClass('fas fa-star');
-		  }
-			$(".addFav").click(function(){	
-				if($(this).parent().parent().parent().attr('id') === 'searchRes'){		    
-					$favItem.append($(this).parent().parent().clone(true, true));
-					$(this).css('color','green');
-					$("#favList #favItem i").css('color','green');
-    		$("#favItem > div").removeDuplicates();
-				}
-
-				else if($(this).parent().parent().parent().attr('id') === 'favItem'){
-					$(this).parent().parent().remove();
-					//$("#searchRes > #" + $(this).attr('id') + " .favItem").css('color','grey');
-					//$("#searchRes > #" + $(this).attr('id') + " i").click(function(){$(this).css('color','grey');});
-				}
-			});
-
-	    }
-	    else {
-		console.log("Error JSON file not found");
-	    }
-	    
-	   });
-  }
-
-  //Search button functions, click anytime to refresh search
+  //Search functions, click button or press enter to refresh search
   $searchButton.click(main);
   $searchField.keypress(function (key) {
 	if (key.which == 13) {
 		main();  
   	}
   });
+
+  var decodeEntities = (function() {
+	  // this prevents any overhead from creating the object each time
+	  var element = document.createElement('div');
+
+	  function decodeHTMLEntities (str) {
+		if(str && typeof str === 'string') {
+			// strip script/html tags
+			str = str.replace(/<script[^>]*>([\S\s]*?)<\/script>/gmi, '');
+			str = str.replace(/<\/?\w(?:[^"'>]|"[^"]*"|'[^']*')*>/gmi, '');
+			element.innerHTML = str;
+			str = element.textContent;
+			element.textContent = '';
+		}
+
+		return str;
+	  }
+
+	  return decodeHTMLEntities;
+  })();
 });
 
 (function($) {
